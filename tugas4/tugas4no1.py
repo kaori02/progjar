78d4f516f4a97f6d38175e77ed3ce6cae330c335
aaronastonvilla <59636803+aaronastonvilla@users.noreply.github.com>
2021-06-24 18:28:26 +0800

    udh bisa lebih dari 1 page, input pagenya udah auto

--- tugas4/tugas4no1.py
+++ tugas4/tugas4no1.py
@@ -7,23 +7,13 @@ titles = []
 def getGoPackage(query, n):
     global pages
 
-    if n <= 10 :
-        pages = 1
-    elif n <= 20 :
-        pages = 2
-    elif n <= 30 :
-        pages = 3
-    elif n <= 40 :
-        pages = 4
-    elif n <= 50 :
-        pages = 5
-    elif n <= 60 :
-        pages = 6
-    else : 
-        print("Invalid input")
+    pages = n/10
+    halaman = (pages-1)*10
+    if halaman > 0 :
+        pages=int(pages)+1
 
     for page in range(pages):
-        page = requests.get("https://pkg.go.dev/search?page=" + str(page) + "&q=" + query)
+        page = requests.get("https://pkg.go.dev/search?page=" + str(page+1) + "&q=" + query)
         soup = BeautifulSoup(page.text, 'html.parser')
         
         filter1 = soup.find_all('div', class_ = 'LegacySearchSnippet')
@@ -41,12 +31,10 @@ query1 = str(input("Query: "))
 number = int(input("Jumlah: "))        
 getGoPackage(query1, number)
     
-
 n=1
 for i,j in zip(titles, urls):
-    while n <= number :
         print(n, "Title = ", i, '\n',"Url = ", j,'\n')
         print("-------------------------------------------------------------------------------------------------")
         n+=1
-            
- 
+        if n==number+1:
+            break
